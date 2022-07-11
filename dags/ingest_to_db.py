557f19fff2bb918d73ffe02cd1de03ee98ce4b14
Enrique Olivares <enrique.olivares@wizeline.com>
2022-07-10 21:25:42 -0500

    Refined ingestion dag

--- dags/ingest_to_db.py
+++ dags/ingest_to_db.py
@@ -10,20 +10,24 @@ from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.sql import BranchSQLOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.trigger_rule import TriggerRule
 
 # General constants
-DAG_ID = "database_ingestion_workflow"
+DAG_ID = "aws_database_ingestion_workflow"
+DATA_TYPE = "sample"
+CLOUD_PROVIDER = "aws"
 
 # AWS constants
 AWS_CONN_ID = "aws_default"
-S3_BUCKET_NAME = ""
+S3_BUCKET_NAME = "bootcamp-project-assets"
 S3_KEY_NAME = "datasets/monthly-charts.csv"
 
 # Postgres constants
-POSTGRES_CONN_ID = "postgres_default"
+POSTGRES_CONN_ID = "ml_conn"
 POSTGRES_TABLE_NAME = "monthly_charts"
 
 
@@ -54,7 +58,12 @@ def ingest_data_from_s3(
         psql_hook.bulk_load(table=postgres_table, tmp_file=tmp.name)
 
 
-with DAG(dag_id=DAG_ID, schedule_interval="@once") as dag:
+with DAG(
+    dag_id=DAG_ID,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    tags=[CLOUD_PROVIDER, DATA_TYPE],
+) as dag:
     start_workflow = DummyOperator(task_id="start_workflow")
 
     verify_key_existence = S3KeySensor(
@@ -84,11 +93,12 @@ with DAG(dag_id=DAG_ID, schedule_interval="@once") as dag:
         """,
     )
 
-    delete_data = PostgresOperator(
-        task_id="create_table_entity",
+    clear_table = PostgresOperator(
+        task_id="clear_table",
         postgres_conn_id=POSTGRES_CONN_ID,
         sql=f"DELETE FROM {POSTGRES_TABLE_NAME}",
     )
+    continue_process = DummyOperator(task_id="continue_process")
 
     ingest_data = PythonOperator(
         task_id="ingest_data",
@@ -100,14 +110,15 @@ with DAG(dag_id=DAG_ID, schedule_interval="@once") as dag:
             "s3_key": S3_KEY_NAME,
             "postgres_table": POSTGRES_TABLE_NAME,
         },
+        trigger_rule=TriggerRule.ONE_SUCCESS,
     )
 
     validate_data = BranchSQLOperator(
         task_id="validate_data",
         conn_id=POSTGRES_CONN_ID,
         sql=f"SELECT COUNT(*) AS total_rows FROM {POSTGRES_TABLE_NAME}",
-        follow_task_ids_if_false=[ingest_data.task_id],
-        follow_task_ids_if_true=[delete_data.task_id],
+        follow_task_ids_if_false=[continue_process.task_id],
+        follow_task_ids_if_true=[clear_table.task_id],
     )
 
     end_workflow = DummyOperator(task_id="end_workflow")
@@ -117,8 +128,8 @@ with DAG(dag_id=DAG_ID, schedule_interval="@once") as dag:
         >> verify_key_existence
         >> create_table_entity
         >> validate_data
-        >> [ingest_data, delete_data >> ingest_data]
-        >> end_workflow
     )
+    validate_data >> [clear_table, continue_process] >> ingest_data
+    ingest_data >> end_workflow
 
     dag.doc_md = __doc__
