057047dbc917ae6f48d21d2ffe5f035e312abd0c
Enrique Olivares <enrique.olivares@wizeline.com>
2022-07-12 18:34:36 -0500

    Added postgres operator

--- dags/ingestion_to_db.py
+++ dags/ingestion_to_db.py
@@ -1,11 +1,16 @@
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 
-with DAG("db_ingestion", start_date=days_ago(1)) as dag:
+with DAG(
+    "db_ingestion", start_date=days_ago(1), schedule_interval="@once"
+) as dag:
     start_workflow = DummyOperator(task_id="start_workflow")
     validate = DummyOperator(task_id="validate")
-    prepare = DummyOperator(task_id="prepare")
+    prepare = PostgresOperator(
+        task_id="prepare", postgres_conn_id="ml_conn", sql=""
+    )
     load = DummyOperator(task_id="load")
     end_workflow = DummyOperator(task_id="end_workflow")
 
