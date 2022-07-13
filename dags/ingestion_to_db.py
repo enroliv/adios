d741a621875f93e18bb82421649e389e3de97918
Enrique Olivares <enrique.olivares@wizeline.com>
2022-07-12 19:12:05 -0500

    Added branching strategy

--- dags/ingestion_to_db.py
+++ dags/ingestion_to_db.py
@@ -1,6 +1,7 @@
 from airflow.models import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.operators.sql import BranchSQLOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
@@ -52,7 +53,21 @@ with DAG(
             )
         """,
     )
+    clear = PostgresOperator(
+        task_id="clear",
+        postgres_conn_id="ml_conn",
+        sql="""DELETE FROM monthly_charts_data""",
+    )
+    continue_workflow = DummyOperator(task_id="continue_workflow")
+    branch = BranchSQLOperator(
+        task_id="is_empty",
+        conn_id="ml_conn",
+        sql="SELECT COUNT(*) AS rows FROM monthly_charts_data",
+        follow_task_ids_if_true=[clear.task_id],
+        follow_task_ids_if_false=[continue_workflow.task_id],
+    )
     load = PythonOperator(task_id="load", python_callable=ingest_data)
     end_workflow = DummyOperator(task_id="end_workflow")
 
-    start_workflow >> validate >> prepare >> load >> end_workflow
+    start_workflow >> validate >> prepare >> branch
+    branch >> [clear, continue_workflow] >> load >> end_workflow
